9081ace0b429dd0d44e851a3ce0713db8bcc09be
Chyavan Shenoy <chyavandshenoy2318@gmail.com>
2024-12-29 11:16:36 +0530

    Added | Function to Initialize and Reuse OpenAI client.

---
+++ resume-server/utils/openai_client.py
@@ -0,0 +1,27 @@
+from openai import OpenAI
+
+_cached_client = None
+
+async def get_openai_client(api_key: str, base_url: str):
+    """
+    Function returns a cached OpenAI client if exists, else creates a new client.
+    Use in Entry point so client can be reused.
+    :return:
+    OpenAI client
+    """
+    global _cached_client
+    if _cached_client is None:
+        if api_key is None:
+            raise ValueError("API key not provide; API key must be provided to create the client")
+        _cached_client = OpenAI(
+            api_key=api_key,
+            base_url=base_url,
+        )
+    return _cached_client
+
+async def get_new_openai_client(api_key, base_url=None):
+    """
+    Always creates a new OpenAI client instance with the provided parameters.
+    Use when client is a one-time need.
+    """
+    return OpenAI(api_key=api_key, base_url=base_url)
