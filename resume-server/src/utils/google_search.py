0831fefa8abdba2dcd380b704cedf48fc02cd647
Chyavan Shenoy <chyavandshenoy2318@gmail.com>
2025-01-01 23:56:06 +0530

    Update | Added Pause to Prevent DoS and Rate Limit

--- resume-server/src/utils/google_search.py
+++ resume-server/src/utils/google_search.py
@@ -1,8 +1,8 @@
 from googlesearch import search
 
-async def search_google(query: dict, lang: str = "en", num: int = 10, stop: int = 10):
+async def search_google(query: dict, lang: str = "en", num: int = 10, stop: int = 10, pause: float = 2.0):
     # loop through the queries and search
     search_results = []
     for q in query:
-        search_results.append(search(q, num=num, lang=lang, stop=stop))
+        search_results.append(search(q, num=num, lang=lang, stop=stop, pause=pause))
     return search_results
